4fab990100ceb170d7c9ad4531371e37bfa828cf
eryktr <eryk.trzeciakiewicz@gmail.com>
2020-10-13 20:40:03 +0200

    bumped patch

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='xmarievm',
-    version='1.3.0',
+    version='1.3.1',
     description='Virtual machine to run XMARIE code',
     author='eryktr',
     install_requires=[
