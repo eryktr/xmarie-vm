e3d897baa46d0ac75780af3da7e7adb135de76ae
eryktr <eryk.trzeciakiewicz@gmail.com>
2020-11-17 18:59:17 +0100

    Fixed another bug in step

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='xmarievm',
-    version='1.8.1',
+    version='1.8.2',
     description='Virtual machine to run XMARIE code',
     author='eryktr',
     install_requires=[
